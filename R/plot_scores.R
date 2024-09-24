6e058e37805f41075d4037558f795a857b96bc04
Adam Mattsson <mattsada@gmail.com>
2024-09-24 12:06:59 +0200

    Updating parameter names in plot_scores

--- R/plot_scores.R
+++ R/plot_scores.R
@@ -8,7 +8,7 @@
 #' prediction scores in a tidy format. To do so, set `return_scores = TRUE`. For a greater explanation
 #' on how to use the function, see parameter descriptions and examples.
 #'
-#' @param this_data A list with a data frame object called scores. Returned with `predict_lundtax`.
+#' @param these_predictions A list with a data frame object called scores. Returned with `predict_lundtax`.
 #' @param out_path Optional, set path to export plot. If not provided, tidy version of incoming
 #' scores in data frame format will be returned (`return_scores` will be auto-defaulted to TRUE).
 #' @param out_format Required parameter if `out_path` is specified. Can be "png" (default) or "pdf".
@@ -25,10 +25,8 @@
 #' @param plot_anno_legend Expects a vector with TRUE/FALSE (7 in total), thsi decides what legends
 #' will be on the final heatmap. Default is to only show the legend for the subtypes.
 #' @param plot_hm_legend Boolean parameter. Set to TRUE to show heatmap legend. Default is FALSE.
-#' @param plot_width If `out_format = "png"`, this parameter controls the width in pixels.
-#' Default is 14 inches (4200 pixels at 300 PPI).
-#' @param plot_height If `out_format = "png"`, this parameter controls the height in pixels.
-#' Default is 6 inches (1800 pixels at 300 PPI)
+#' @param plot_width This parameter controls the width in inches. Default is 14(4200 pixels at 300 PPI).
+#' @param plot_height This parameter controls the height in inches.Default is 6(1800 pixels at 300 PPI)
 #' @param plot_font_size Optional parameter to control the size of the font in the generated heatmap.
 #' Note, the title of the plot will always be twice that of the set value here (default = 12).
 #' @param verbose Set to TRUE for debugging purposes. Default is FALSE.
@@ -45,18 +43,18 @@
 #' #load pacakges
 #' library(ComplexHeatmap, ggplot2, grid, circlize, openxlsx, grDevices, utils)
 #' 
-#' my_predictions = predict_lundtax(this_data = sjodahl_2017, 
+#' my_predictions = predict_lundtax(these_predictions = sjodahl_2017, 
 #'                                  gene_id = "hgnc_symbol", 
 #'                                  impute = TRUE, 
 #'                                  adjust = TRUE)
 #' 
-#' plot_scores(this_data = my_predictions,
+#' plot_scores(these_predictions = my_predictions,
 #'             out_path = "../",
 #'             out_format = "pdf",
 #'             title = "Lund2017")
 #'}
 #'
-plot_scores = function(this_data = NULL,
+plot_scores = function(these_predictions = NULL,
                        out_path = NULL,
                        out_format = "png",
                        return_scores = FALSE,
@@ -88,7 +86,7 @@ plot_scores = function(this_data = NULL,
     }
 
   #check the type of incoming data
-  if(is.null(this_data) | !is.list(this_data)){
+  if(is.null(these_predictions) | !is.list(these_predictions)){
     stop("Input should be the result of applying predict_lundtax")
   }
 
@@ -112,13 +110,13 @@ plot_scores = function(this_data = NULL,
     }
 
     #set sample order
-    sample_order <- order(this_data$scores$proliferation_score)
+    sample_order <- order(these_predictions$scores$proliferation_score)
 
     #set split (top annotation track of heatmap)
     if(!is.null(hm_split)){
       split = hm_split
       }else{
-        split <- factor(this_data$predictions_7classes,
+        split <- factor(these_predictions$predictions_7classes,
                         levels = c("UroA","UroB","UroC","GU","BaSq","Mes","ScNE"))
      }
 
@@ -136,16 +134,16 @@ plot_scores = function(this_data = NULL,
     #set colours
     #proliferation
     col_fun_proliferation =
-      circlize::colorRamp2(c(quantile(this_data$scores$proliferation_score, 0.05),
-                             median(this_data$scores$proliferation_score),
-                             quantile(this_data$scores$proliferation_score, 0.95)),
+      circlize::colorRamp2(c(quantile(these_predictions$scores$proliferation_score, 0.05),
+                             median(these_predictions$scores$proliferation_score),
+                             quantile(these_predictions$scores$proliferation_score, 0.95)),
                            c("#21908CFF","white", "#B63679FF"))
 
     #progression
     col_fun_progression =
-      circlize::colorRamp2(c(quantile(this_data$scores$progression_score, 0.05),
-                             median(this_data$scores$progression_score),
-                             quantile(this_data$scores$progression_score, 0.90)),
+      circlize::colorRamp2(c(quantile(these_predictions$scores$progression_score, 0.05),
+                             median(these_predictions$scores$progression_score),
+                             quantile(these_predictions$scores$progression_score, 0.90)),
                            c("#FAEBDDFF","#A11A5BFF", "#4C1D4BFF"))
 
     #create colour object
@@ -159,11 +157,11 @@ plot_scores = function(this_data = NULL,
     #plotting
     #build heatmap annotation (top)
     hm <- HeatmapAnnotation(lund_subtype = split,
-                            proliferation_score = this_data$scores$proliferation_score,lund_subtype = split,
-                            molecular_grade_who_1999 = this_data$scores$molecular_grade_who_1999,
-                            molecular_grade_who_2016 = this_data$scores$molecular_grade_who_2016,
-                            progression_score = this_data$scores$progression_score,
-                            progression_risk = this_data$scores$progression_risk,
+                            proliferation_score = these_predictions$scores$proliferation_score,lund_subtype = split,
+                            molecular_grade_who_1999 = these_predictions$scores$molecular_grade_who_1999,
+                            molecular_grade_who_2016 = these_predictions$scores$molecular_grade_who_2016,
+                            progression_score = these_predictions$scores$progression_score,
+                            progression_risk = these_predictions$scores$progression_risk,
                             annotation_name_side = "left",
                             show_legend = plot_anno_legend,
                             annotation_name_gp = gpar(fontsize = plot_font_size),
@@ -173,11 +171,11 @@ plot_scores = function(this_data = NULL,
                             row_title = NULL)
 
     #immune scores heatmap
-    hm_immune_scores <- Heatmap(t(scale(this_data$scores[,immune_names, drop = FALSE])),
+    hm_immune_scores <- Heatmap(t(scale(these_predictions$scores[,immune_names, drop = FALSE])),
                                 top_annotation = hm,
                                 column_order = sample_order,
-                                height = unit(5*ncol(this_data$scores[,immune_names,drop = FALSE]), "mm"),
-                                width = unit(0.5*nrow(this_data$scores[,immune_names,drop = FALSE]), "mm"),
+                                height = unit(5*ncol(these_predictions$scores[,immune_names,drop = FALSE]), "mm"),
+                                width = unit(0.5*nrow(these_predictions$scores[,immune_names,drop = FALSE]), "mm"),
                                 name = "Immune Scores",
                                 border = TRUE,
                                 column_split = split,
@@ -190,10 +188,10 @@ plot_scores = function(this_data = NULL,
                                 row_title = NULL)
 
     #stroma scores heatmap
-    hm_stroma_scores <- Heatmap(t(scale(this_data$scores[,stromal_names, drop = FALSE])),
+    hm_stroma_scores <- Heatmap(t(scale(these_predictions$scores[,stromal_names, drop = FALSE])),
                                 column_order = sample_order,
-                                height = unit(5*ncol(this_data$scores[,stromal_names, drop = FALSE]), "mm"),
-                                width = unit(0.5*nrow(this_data$scores[,stromal_names,drop = FALSE]), "mm"),
+                                height = unit(5*ncol(these_predictions$scores[,stromal_names, drop = FALSE]), "mm"),
+                                width = unit(0.5*nrow(these_predictions$scores[,stromal_names,drop = FALSE]), "mm"),
                                 name = "Stroma Scores",
                                 border = TRUE,
                                 column_split = split,
@@ -226,7 +224,7 @@ plot_scores = function(this_data = NULL,
   if(return_scores == TRUE){
 
     #subset scores data frame from list
-    my_scores = this_data$scores
+    my_scores = these_predictions$scores
     my_scores = as.data.frame(my_scores)
 
     #add new column with cohort information

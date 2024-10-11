60fdaa64b1ced3776da5aa2032455216bed7f8b8
mattssca <mattsada@gmail.com>
2024-10-11 21:08:40 +0200

    Adding new plotting functions for visualizing subtype scores

---
+++ R/plot_subscore_box.R
@@ -0,0 +1,138 @@
+#' @title Plot Stacked Barplot Subtype Score
+#'
+#' @description Visualize subtype prediction score within a set subtype in a stacked barplot.
+#'
+#' @details Take the output from `lundtax_predict_sub` and return a stacked barplot plot representing 
+#' the distribution of the, for that class, subtype prediction score. Set the subtype of 
+#' desire with `this_subtype`. The subtype can be one of the subtypes included in the LundTax subtype 
+#' classification nomenclature.
+#'
+#' @param these_predictions Required parameter, should be the output from 
+#' [LundTax2023Classifier::lundtax_predict_sub()].
+#' @param this_subtype Required parameter. Should be one of the set subtype classes from the
+#'  LundTax nomenclature.
+#'
+#' @return Nothing.
+#' 
+#' @import ggplot2 dplyr reshape2 gridExtra
+#'
+#' @export
+#'
+#' @examples
+#' #' my_predictions = lundtax_predict_sub(these_predictions = sjodahl_2017, 
+#'                                      gene_id = "hgnc_symbol", 
+#'                                      impute = TRUE, 
+#'                                      adjust = TRUE)
+#'                                      
+#' uro_scores = plot_subscore_box(these_predictions = my_predictions, 
+#'                                this_subtype = "Uro")
+#'
+plot_subscore_box = function(these_predictions, 
+                             this_subtype){
+  
+  #subset scores
+  these_scores = data.frame(these_predictions$subtype_scores)
+  
+  #get subtypes based on subtype class
+  if(this_subtype %in% c("Uro", "GU", "BaSq", "Mes", "ScNE")){
+    these_subtypes = data.frame(these_predictions$predictions_5classes)
+    subtype_classes = c("Uro", "GU", "BaSq", "Mes", "ScNE")
+  }else if(this_subtype %in% c("UroA", "UroB", "UroC")){
+    these_subtypes = data.frame(these_predictions$predictions_7classes)
+    subtype_classes = c("UroA", "UroB", "UroC")
+  }
+  
+  #cnvert rowname to columns
+  these_scores = tibble::rownames_to_column(these_scores, "sample_id")
+  these_subtypes = tibble::rownames_to_column(these_subtypes, "sample_id")
+  
+  #rename column
+  colnames(these_subtypes)[2] = "subtype"
+  
+  #subset consensus subtypes
+  these_samples = dplyr::filter(these_subtypes, subtype == this_subtype)
+  
+  #replace NAs with zero
+  these_scores[is.na(these_scores)] <- 0
+  
+  #melt data frame
+  melted_scores = melt(data = these_scores,
+                       id = "sample_id",
+                       variable.name = "subtype",
+                       measure.vars = c("Uro",
+                                        "UroA",
+                                        "UroB",
+                                        "UroC",
+                                        "GU",
+                                        "BaSq",
+                                        "Mes",
+                                        "ScNE"))
+  
+  #convert to factor
+  melted_scores$subtype = as.factor(melted_scores$subtype) 
+  
+  #subset on class
+  class_melted = melted_scores %>%  
+    dplyr::filter(subtype %in% subtype_classes)
+  
+  #get sample IDs for each subtype
+  this_melted = dplyr::filter(class_melted, sample_id %in% these_samples$sample_id)
+  
+  #get colour for the subtype to be plotted
+  subtype_col = lund_colors$lund_colors[this_subtype]
+  
+  #filter the data frame to get rows where subtype is the selected subtype
+  subtype_df = this_melted[this_melted$subtype == this_subtype, ]
+  
+  #sort these rows by the value in descending order
+  subtype_df = subtype_df[order(-subtype_df$value), ]
+  
+  #for each sample_id, sort the subtypes by their values in ascending order
+  sorted_df = do.call(rbind, lapply(unique(subtype_df$sample_id), function(sample) {
+    sample_df = this_melted[this_melted$sample_id == sample, ]
+    sample_df = sample_df[order(sample_df$value), ]
+    return(sample_df)
+  }))
+  
+  #combine the sorted data frames
+  sorted_df = sorted_df[order(match(sorted_df$sample_id, subtype_df$sample_id)), ]
+  
+  #reorder the sample_id factor levels based on the sorted subtype_df
+  sorted_df$sample_id = factor(sorted_df$sample_id, levels = subtype_df$sample_id)
+  
+  #ensure the selected subtype's values are at the bottom
+  sorted_df = sorted_df %>%
+    group_by(sample_id) %>%
+    arrange(desc(subtype == this_subtype), value, .by_group = TRUE) %>%
+    ungroup()
+  
+  #reorder the subtype factor levels to ensure the selected subtype is plotted first
+  sorted_df$subtype = factor(sorted_df$subtype, levels = c(this_subtype, setdiff(levels(sorted_df$subtype), this_subtype)))
+  
+  #get colour for the subtype to be plotted
+  subtype_col = lund_colors$lund_colors[this_subtype]
+
+  #plot the sorted data frame with stacked bar plots
+  my_plot = ggplot(sorted_df, aes(x = sample_id, y = value, fill = subtype)) +
+    geom_col(width = 1) +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+    coord_cartesian(clip = "off") +
+    ylab(this_subtype) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0,1), breaks = seq(0, 1, by = 0.5)) +
+    theme(legend.position = "none", 
+          axis.text.x = element_blank(),
+          axis.text.y = element_text(color = "black", size = 7),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(linewidth = 0.4),
+          axis.title.x = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.4),
+          plot.background = element_blank(), 
+          axis.line.x = element_blank(), 
+          axis.title.y = element_text(angle = 90, colour = "black", size = 10, vjust = 2))
+  
+  return(my_plot)
+}
---
+++ R/plot_subscore_violin.R
@@ -0,0 +1,127 @@
+#' @title Violin Plot Subtype Scores
+#'
+#' @description Visualize subtype prediction score for a set subtype in a violin plot.
+#'
+#' @details Take the output from `lundtax_predict_sub` and return a violin plot where each violin 
+#' represent the distribution of the, for that class, subtype prediction score. Set the subtype of 
+#' desire with `this_subtype`. The subtype can be one of the subtypes included in the LundTax subtype 
+#' classification nomenclature. Parameters starting with the plot prefix are available for the user 
+#' to gain more control of the returned plot. The user can also return the wrangled data used in the 
+#' plot by setting `return_data = TRUE`.
+#'
+#' @param these_predictions Required parameter, should be the output from 
+#' [LundTax2023Classifier::lundtax_predict_sub()].
+#' @param this_subtype Required parameter. Should be one of the set subtype classes from the
+#'  LundTax nomenclature.
+#' @param plot_adjust A multiplicate bandwidth adjustment. This makes it possible to adjust the 
+#' bandwidth while still using the a bandwidth estimator. For example, adjust = 1/2 means use half 
+#' of the default bandwidth.
+#' @param plot_scale if "area" (default), all violins have the same area (before trimming the tails). 
+#' If "count", areas are scaled proportionally to the number of observations. If "width", all 
+#' violins have the same maximum width.
+#' @param return_plot Set to TRUE to return tidy data used by the plotting function. Default is FALSE.
+#'
+#' @return Nothing.
+#' 
+#' @import ggplot2 dplyr reshape2 gridExtra
+#'
+#' @export
+#'
+#' @examples
+#' my_predictions = lundtax_predict_sub(these_predictions = sjodahl_2017, 
+#'                                      gene_id = "hgnc_symbol", 
+#'                                      impute = TRUE, 
+#'                                      adjust = TRUE)
+#'                                      
+#' uro_scores = plot_subscore_violin(these_predictions = my_predictions, 
+#'                                   this_subtype = "Uro")
+#'                                   
+plot_subscore_violin = function(these_predictions, 
+                                this_subtype, 
+                                plot_adjust = 2,
+                                plot_scale = "area",
+                                return_scores = FALSE){
+  
+  #subset scores
+  these_scores = data.frame(these_predictions$subtype_scores)
+  
+  #get subtypes based on subtype class
+  if(this_subtype %in% c("Uro", "GU", "BaSq", "Mes", "ScNE")){
+    these_subtypes = data.frame(these_predictions$predictions_5classes)
+    subtype_classes = c("Uro", "GU", "BaSq", "Mes", "ScNE")
+  }else if(this_subtype %in% c("UroA", "UroB", "UroC")){
+    these_subtypes = data.frame(these_predictions$predictions_7classes)
+    subtype_classes = c("UroA", "UroB", "UroC")
+  }
+
+  #convert rowname to columns
+  these_scores = tibble::rownames_to_column(these_scores, "sample_id")
+  these_subtypes = tibble::rownames_to_column(these_subtypes, "sample_id")
+
+  #rename column
+  colnames(these_subtypes)[2] = "subtype"
+
+  #subset consensus subtypes
+  these_samples = dplyr::filter(these_subtypes, subtype == this_subtype)
+
+  #replce NAs with zero
+  these_scores[is.na(these_scores)] <- 0
+  
+  #melt data frame
+  melted_scores = melt(data = these_scores,
+                       id = "sample_id",
+                       variable.name = "subtype",
+                       measure.vars = c("Uro",
+                                        "UroA",
+                                        "UroB",
+                                        "UroC",
+                                        "GU",
+                                        "BaSq",
+                                        "Mes",
+                                        "ScNE"))
+  
+  #convert to factor
+  melted_scores$subtype = as.factor(melted_scores$subtype) 
+  
+  #subset on class
+  class_melted = melted_scores %>%  
+    dplyr::filter(subtype %in% subtype_classes)
+
+  #get sample IDs for each subtype
+  this_melted = dplyr::filter(class_melted, sample_id %in% these_samples$sample_id)
+  
+  #get colour for the subtype to be plotted
+  subtype_col = lund_colors$lund_colors[this_subtype]
+  
+  if(return_scores){
+    message("Returning melted data frame with scores...")
+    return(this_melted)
+  }
+
+  #plot subtype scores
+  my_plot = this_melted %>% 
+    arrange(value) %>%    
+    mutate(subtype = factor(subtype, levels = c("Uro", "GU", "BaSq", "Mes", "ScNE", "UroA", "UroB", "UroC"))) %>% 
+    ggplot(aes(x = subtype, y = value, fill = subtype), show.legend = TRUE) + 
+    geom_violin(scale = plot_scale, trim = TRUE, color = NA, adjust = plot_adjust) +
+    scale_fill_manual(values = lund_colors$lund_colors, drop = FALSE) +
+    theme_bw() +
+    coord_cartesian(clip = "off") +
+    ylab(this_subtype) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0,1), breaks = seq(0, 1, by = 0.5)) +
+    theme(legend.position = "none", 
+          axis.text.x = element_blank(),
+          axis.text.y = element_text(color = "black", size = 7),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(linewidth = 0.4),
+          axis.title.x = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.background = element_blank(), 
+          panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.4),
+          axis.line.x = element_blank(), 
+          axis.title.y = element_text(angle = 90, colour = "black", size = 10, vjust = 2))
+  
+  return(my_plot)
+}
